787fceadb5b96f9c6b8d7ad8973627aff88b1e07
lawff <evilllawliet@gmail.com>
2024-06-11 11:03:17 +0800

    fix: console-subscriber

--- examples/chat.rs
+++ examples/chat.rs
@@ -36,6 +36,8 @@ async fn main() -> Result<()> {
     let layer = Layer::new().with_filter(LevelFilter::INFO);
     tracing_subscriber::registry().with(layer).init();
 
+    // console_subscriber::init();
+
     let addr = "0.0.0.0:8080";
     let listener = TcpListener::bind(addr).await?;
     info!("Listening on: {}", addr);
